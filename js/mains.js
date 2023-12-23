69fcabd2b201e6968730ab3484ca3b20f8a848eb
Jose Mauricio Piron <contato.mauriciopiron@gmail.com>
2023-12-23 13:31:59 -0300

    First commit

---
+++ js/mains.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+        
+    
+    $('#telefone').mask('(00) 00000-0000')
+
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            mensagem: {
+                mensagem: false
+            },
+        
+    },
+    messages: {
+        nome:      'Insira seu nome completo',
+        email:     'Informe seu endereço de e-mail', 
+        telefone:  'Informe número de telefone'
+        
+    },
+    submitHandler:function(form){
+        console.log(form)
+    },
+    invalidHandler: function(evento, validador){
+        let camposIncorretos = validador.numberOfInvalids();
+        if(camposIncorretos) {
+            alert(`Existem ${camposIncorretos} campos incorretos`)
+        }
+        
+    }
+})
+})
