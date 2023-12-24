5de5c0b190d2a6425f7b7fa69a79ba818ea243f9
Jose Mauricio Piron <contato.mauriciopiron@gmail.com>
2023-12-23 21:08:11 -0300

    First commit

--- main.js
+++ main.js
@@ -1,28 +1,41 @@
-document.addEventListener('DOMContentLoaded', function(){
-    const endpoint = `https://api.github.com/users/Mauricio-Piron`
-    
-    fetch(endpoint).then(function(resposta){
-        return resposta.json()
-    })
-    .then(function(json){
-        const avatar = json.avatar_url
-        $('#avatar').attr("src", avatar);
-        
-        
-        const nome = json.login
-        $('#name').html(`@${nome}`)
+function Conta(cliente){  //Abstração 
+    this.cliente = cliente;
 
-        const repositorio = json.public_repos
-        $('#repositories').html(`${repositorio}`)
+    this.mensagem =  function () {
+        console.log(`Seja bem vindo a sua conta ${this.cliente} `)
+    };
+}
 
-        const seguidores = json.followers
-        $('#followers').html(seguidores)
+function ContaCorrente(cliente, numeroContaC, saldoC) { //herança classe Conta 
+    this.cliente = cliente;
+    this.numeroContaC = numeroContaC;
+    this.saldoC = saldoC;
 
-        const seguindo = json.following
-        $('#following').html(seguindo)
+    Conta.call(this, cliente )
+}   
 
-        const link = json.html_url
-        console.log(link)
-        $('#link').attr("href", link)
-    })
-})
+function ContaPoupanca(cliente, numeroContaP, saldoP){ //herança classe Conta 
+    this.cliente = cliente;
+    this.numeroContaP = numeroContaP;
+    this.saldoP = saldoP;
+
+    Conta.call(this, cliente)
+}
+const cliente = new Conta("Aparecida" , "753697");
+
+const ContaCorrente1 = new ContaCorrente("Mauricio", "12345", "5000") 
+const ContaPoupanca1 = new ContaPoupanca ("Mauricio", "54321", "1000");
+const ContaCorrente2 = new ContaCorrente("José", "147258", "1000");
+const ContaPoupanca2 = new ContaPoupanca("José", "258147", "5000");
+
+console.log(cliente);
+
+console.log(ContaCorrente1);
+ContaCorrente1.mensagem();
+console.log(ContaPoupanca1);
+ContaPoupanca1.mensagem()
+
+console.log(ContaCorrente2);
+ContaCorrente2.mensagem();
+console.log(ContaPoupanca2);
+ContaPoupanca2.mensagem()
