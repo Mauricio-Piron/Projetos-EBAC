d3a7d1dd5753ee1b23419e2859dde739335100a3
Jose Mauricio Piron <contato.mauriciopiron@gmail.com>
2023-12-24 12:05:13 -0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,44 @@
-import Cabecalho from './components/Cabecalho'
-import Header from './components/Cabecalho'
-import Hero from './components/Hero'
-import ListaVagas from './containers/ListaVagas'
-import EstiloGlobal, { Container } from './global'
+import { useEffect, useState } from 'react'
+import Header from './components/Header'
+import Produtos from './containers/Produtos'
+
+import { GlobalStyle } from './styles'
+
+export type Game = {
+  id: number
+  titulo: string
+  plataformas: string[]
+  precoAntigo: number
+  preco: number
+  categoria: string
+  imagem: string
+}
 
 function App() {
+  const [games, setGames] = useState<Game[]>([])
+  const [carrinho, setCarrinho] = useState<Game[]>([])
+
+  useEffect(() => {
+    fetch('http://localhost:4000/produtos')
+      .then((res) => res.json())
+      .then((res) => setGames(res))
+  }, [])
+
+  function adicionarAoCarrinho(jogo: Game) {
+    if (carrinho.find((game) => game.id === jogo.id)) {
+      alert('Item já adicionado')
+    } else {
+      setCarrinho([...carrinho, jogo])
+    }
+  }
+
   return (
     <>
-      <EstiloGlobal />
-      <Cabecalho />
-      <Hero />
-      <Container>
-        <ListaVagas />
-      </Container>
+      <GlobalStyle />
+      <div className="container">
+        <Header itensNoCarrinho={carrinho} />
+        <Produtos jogos={games} adicionarAoCarrinho={adicionarAoCarrinho} />
+      </div>
     </>
   )
 }
--- src/components/Cabecalho/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Cabecalho as CabecalhoEstilo } from './styles'
-
-const Cabecalho = () => (
-  <header>
-    <CabecalhoEstilo>EBAC Jobs</CabecalhoEstilo>
-  </header>
-)
-
-export default Cabecalho
